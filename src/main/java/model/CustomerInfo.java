6d98e1914f99b0d15db90638a90c3e54b61df586
PasiSandeepa <pasisandeepa456@gmail.com>
2025-09-23 16:30:07 +0530

    Create Customer model class

---
+++ src/main/java/model/CustomerInfo.java
@@ -0,0 +1,24 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CustomerInfo {
+    private String custID;
+    private String custTitle;
+    private String custName;
+    private String dob;
+    private Double salary;
+    private String custAddress;
+    private String city;
+    private String province;
+    private String postalCode;
+    private String custPassword;
+
+}
